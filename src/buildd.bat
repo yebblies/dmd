9d0dcb27e8f35eb94be78ef67d8b08056bc7d17e
Daniel Murphy <yebblies@gmail.com>
2012-01-24 00:05:01 +1100

    root\dchar

--- src/buildd.bat
+++ src/buildd.bat
@@ -3,10 +3,9 @@
 
 rem **** Build c/c++ source files ****
 dmc -c -Iroot;\dm\include -D -g -DUNITTEST -cpp root\aav.c
+dmc -c -Iroot;\dm\include -D -g -DUNITTEST -cpp root\dchar.c
 
-dmd -c testroot
-
-dmd testroot root\aav.obj
+dmd testroot root\aav aav.obj root\_dchar dchar.obj
 
 testroot
 
