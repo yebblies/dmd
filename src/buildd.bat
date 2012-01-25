df9b0b73772af94be9e0c70a9ef17be55183a627
Daniel Murphy <yebblies@gmail.com>
2012-01-25 12:41:52 +1100

    Don't build c++ objects manually

--- src/buildd.bat
+++ src/buildd.bat
@@ -1,20 +1,7 @@
 
 @echo off
 
-rem **** Build c/c++ source files ****
-dmc -c -Iroot;\dm\include -D -g -DUNITTEST -cpp root\aav.c
-dmc -c -Iroot;\dm\include -D -g -DUNITTEST -cpp root\dchar.c
-dmc -c -Iroot;\dm\include -D -g -DUNITTEST -cpp root\lstring.c
-dmc -c -Iroot;\dm\include -D -g -DUNITTEST -cpp root\rmem.c
-dmc -c -Iroot;\dm\include -D -g -DUNITTEST -cpp root\port.c
-dmc -c -Iroot;\dm\include -D -g -DUNITTEST -cpp root\stringtable.c
-dmc -c -Iroot;\dm\include -D -g -DUNITTEST -cpp root\root.c
-dmc -c -Iroot;\dm\include -D -g -DUNITTEST -cpp root\array.c
-dmc -c -Iroot;\dm\include -D -g -DUNITTEST -cpp root\gnuc.c
-dmc -c -Iroot;\dm\include -D -g -DUNITTEST -cpp root\man.c
-dmc -c -Iroot;\dm\include -D -g -DUNITTEST -cpp root\response.c
-dmc -c -Iroot;\dm\include -D -g -DUNITTEST -cpp root\async.c
-dmc -c -Iroot;\dm\include -D -g -DUNITTEST -cpp root\speller.c
+rem compile d source and link with compiler objs
 
 dmd testroot ^
     root\aav root\_dchar root\lstring root\rmem root\root root\speller root\stringtable root\thread ^
