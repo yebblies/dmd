0fb09f5285529777928b40a60a0a46f314ddf3ee
Daniel Murphy <yebblies@gmail.com>
2013-03-03 19:25:21 +1100

    No implicit real -> creal

--- src/expression.c
+++ src/expression.c
@@ -1568,7 +1568,7 @@ complex_t Expression::toComplex()
 #ifdef IN_GCC
     return complex_t(real_t(0)); // %% nicer
 #else
-    return 0.0;
+    return 0.0 + 0 * I;
 #endif
 }
 
@@ -2212,7 +2212,7 @@ real_t IntegerExp::toImaginary()
 
 complex_t IntegerExp::toComplex()
 {
-    return toReal();
+    return toReal() + 0 * I;
 }
 
 int IntegerExp::isBool(int result)
