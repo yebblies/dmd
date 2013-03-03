35218b255654bdc47f31aa887ef9197e22d372a6
Daniel Murphy <yebblies@gmail.com>
2013-03-03 23:54:35 +1100

    char* -> const char*

--- src/statement.c
+++ src/statement.c
@@ -2212,7 +2212,7 @@ Lagain:
                 /* Call:
                  *      _aApply(aggr, flde)
                  */
-                static char fntab[9][3] =
+                static const char fntab[9][3] =
                 { "cc","cw","cd",
                   "wc","cc","wd",
                   "dc","dw","dd"
