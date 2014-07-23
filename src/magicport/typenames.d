21fdd56adf1e6e2400efc6981d3ad2719bb2a515
Daniel Murphy <yebblies@gmail.com>
2014-07-23 21:53:09 +1000

    Add magicport source to repo

---
+++ src/magicport/typenames.d
@@ -0,0 +1,14 @@
+
+bool[string] basicTypes;
+bool[string] structTypes;
+bool[string] classTypes;
+bool[string] rootClasses;
+string[][] overridenFuncs;
+string[] nonFinalClasses;
+
+bool lookup(bool[string] aa, string n)
+{
+    auto p = n in aa;
+    if (p) *p = true;
+    return p !is null;
+}
