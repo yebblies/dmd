eec83171a381110a2903f564fc6e0c608d2a503f
Daniel Murphy <yebblies@gmail.com>
2015-03-08 21:57:38 +1100

    while {} magic

--- src/magicport/dprinter.d
+++ src/magicport/dprinter.d
@@ -1582,11 +1582,26 @@ class DPrinter : Visitor
         print("while (");
         visitX(ast.e);
         println(")");
-        if (!cast(CompoundStatement)ast.sbody)
-            indent++;
-        visitX(ast.sbody);
-        if (!cast(CompoundStatement)ast.sbody)
-            indent--;
+        auto es = cast(ExpressionStatement)ast.sbody;
+        if (es && !es.e)
+        {
+            println("{");
+            if (es.trailingcomment)
+            {
+                indent++;
+                println(es.trailingcomment);
+                indent--;
+            }
+            println("}");
+        }
+        else
+        {
+            if (!cast(CompoundStatement)ast.sbody)
+                indent++;
+            visitX(ast.sbody);
+            if (!cast(CompoundStatement)ast.sbody)
+                indent--;
+        }
     }
 
     override void visit(DoWhileStatement ast)
