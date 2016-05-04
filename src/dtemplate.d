d51be3f2a19b4f905c09902cf984023dae3dc193
Daniel Murphy <yebblies@gmail.com>
2016-05-04 11:06:39 +0200

    Update

--- src/dtemplate.d
+++ src/dtemplate.d
@@ -6710,7 +6710,7 @@ public:
         return buf.extractString();
     }
 
-    override final char* toPrettyCharsHelper()
+    override final const(char)* toPrettyCharsHelper()
     {
         OutBuffer buf;
         toCBufferInstance(this, &buf, true);
